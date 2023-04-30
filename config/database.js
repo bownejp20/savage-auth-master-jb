817606367ddf7cbc611da380762442991d773311
bownejp20 <jbowne24@gmail.com>
2023-04-30 16:05:26 -0400

    fixed connection problem

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : '"mongodb+srv://jbowne24:paige24jb@cluster0.tkio3fd.mongodb.net/demo?retryWrites=true&w=majority"', 
+    'url' : "mongodb+srv://jbowne24:paige24jb@cluster0.tkio3fd.mongodb.net/demo?retryWrites=true&w=majority", 
     'dbName': 'demo'
 };
