e64ab60d424aa0e2ed8e1c5f485cedc64e81eed2
bownejp20 <jbowne24@gmail.com>
2023-04-30 14:55:11 -0400

    first push

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : '"mongodb+srv://jbowne24:paige24jb@cluster0.tkio3fd.mongodb.net/demo?retryWrites=true&w=majority"', 
+    'dbName': 'demo'
+};
